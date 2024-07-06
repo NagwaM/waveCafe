9aafa636bc2a8d30d8b6b46b7f1e71d32e7ca12d
NagwaM <nagwaanwar063@gmail.com>
2024-07-06 15:22:32 +0300

    First commit

---
+++ app/Http/Controllers/FrontPages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontPages extends Controller
+{
+    public function index(){
+        $title = "Wave Cafe Website";
+        return view('home', compact('title'));
+    }
+}
